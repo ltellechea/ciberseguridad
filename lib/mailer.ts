8dcb4ad6cd34d61235a076e53a2d827f162cb227
Leon <ltellechea@atilra.org.ar>
2026-05-19 16:28:40 -0300

    Correccion envio por correo electronico

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -4,14 +4,13 @@ import { join } from 'path';
 
 function createTransport() {
   return nodemailer.createTransport({
-    host: 'smtp-mail.outlook.com',
+    host: 'smtp.gmail.com',
     port: 587,
     secure: false,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
     },
-    tls: { ciphers: 'SSLv3' },
   });
 }
 
